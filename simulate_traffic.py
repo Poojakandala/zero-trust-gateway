ed98f78704a85bacff02cc468e2e4c3b8e1cefb8
coolCoder123-del <balarajugurram77@gmail.com>
2026-02-26 19:55:43 +0530

    Update simulate_traffic.py

--- simulate_traffic.py
+++ simulate_traffic.py
@@ -2,35 +2,33 @@ import requests
 import time
 import random
 
-# Make sure your Gateway is running on Port 8000!
-GATEWAY_URL = "http://localhost:8000/process-transaction"
+GATEWAY_URL = "http://localhost:8000/secure-data"
+HEADERS = {"Authorization": "Bearer MOCK_TOKEN_XYZ"}
 
 def send_request(mode="safe"):
-    headers = {"Authorization": "Bearer MOCK_TOKEN_XYZ"}
     
     if mode == "safe":
-        payload = {"amount": random.randint(10, 100), "user_id": "user_123", "hour": 14}
-        delay = 1 
+        delay = random.uniform(1, 2)
     else:
-        payload = {"amount": 9999, "user_id": "attacker_666", "hour": 2}
-        delay = 0.2 
+        delay = 0.1  # attack traffic = high frequency
 
     try:
-        # This sends the data to your FastAPI Gateway
-        response = requests.post(GATEWAY_URL, json=payload, headers=headers)
-        print(f"[{mode.upper()}] Status: {response.status_code} | Risk: {response.json().get('risk_score', 'N/A')}")
+        response = requests.get(GATEWAY_URL, headers=HEADERS)
+
+        print(f"[{mode.upper()}] Status: {response.status_code} | "
+              f"Risk Score: {response.headers.get('X-Risk-Score', 'N/A')}")
+
     except Exception as e:
-        print(f"❌ Connection Failed: Is your Gateway (Terminal 1) running?")
+        print(f"Connection Failed: {e}")
 
     time.sleep(delay)
 
+
 if __name__ == "__main__":
-    print("🚀 Starting Demo Traffic... Watch your Dashboard!")
-    
-    print("\n--- Sending Normal Traffic ---")
+    print("Sending SAFE traffic...\n")
     for _ in range(5):
         send_request("safe")
-    
-    print("\n🚨 INITIATING ATTACK...")
-    for _ in range(15):
+
+    print("\nSending ATTACK traffic...\n")
+    for _ in range(50):
         send_request("attack")
